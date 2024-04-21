c13769f586824a69e3715e418369c41642316973
Oscar Villemaud <oscar.villemaud@epfl.ch>
2024-04-21 13:13:07 +0200

    testing commit message

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -24,6 +24,8 @@ return {
     'tpope/vim-fugitive',
     config = function()
       vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+      vim.keymap.set('n', '<leader>ga', ':Git add %')
+      vim.keymap.set('n', '<leader>gc', vim.cmd.Git)
 
       local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup('ThePrimeagen_Fugitive', {})
 
