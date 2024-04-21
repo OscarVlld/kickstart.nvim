1d3510e38797ab1ae59f541fb040e82ec9cfc4fc
Oscar Villemaud <oscar.villemaud@epfl.ch>
2024-04-21 17:32:39 +0200

    remove rebase parameter of pull because it bugs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -46,7 +46,7 @@ return {
 
           -- rebase always
           vim.keymap.set('n', '<leader>P', function()
-            vim.cmd.Git { 'pull', '--rebase' }
+            vim.cmd.Git { 'pull' }
           end, opts)
 
           -- NOTE: It allows me to easily set the branch i am pushing and any tracking
