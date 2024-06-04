1bbe645fffea4f199b0beb35d16cb9b4fa0bf0c5
Oscar Villemaud <oscar.villemaud@epfl.ch>
2024-06-04 20:53:21 +0200

    Commented treesitter options to get latex highlight from vimtex

--- init.lua
+++ init.lua
@@ -838,19 +838,20 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
+    -- opts = {
+    --    ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+    --    -- Autoinstall languages that are not installed
+    --    auto_install = true,
+    --    highlight = {
+    --      enable = false,
+    --      disable = { 'latex' },
+    --      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+    --      --  If you are experiencing weird indenting issues, add the language to
+    --      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+    --      additional_vim_regex_highlighting = { 'ruby' },
+    --   },
+    --   indent = { enable = false, disable = { 'ruby' } },
+    -- },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
