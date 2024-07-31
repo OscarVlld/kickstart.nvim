462a32be16789df0977813a39efecadfb79df3bf
Oscar Villemaud <oscar.villemaud@epfl.ch>
2024-07-31 18:23:27 +0200

    add zen.nvim

---
+++ lua/custom/plugins/zen.lua
@@ -0,0 +1,12 @@
+-- ZEN
+
+vim.keymap.set('n', '<Leader>z', ':ZenMode<CR>', { desc = '[Z]en mode' })
+
+return {
+  'folke/zen-mode.nvim',
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+}
