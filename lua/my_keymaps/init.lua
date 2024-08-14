b91ca61800814bb894c77b00c0f128f42054c1b4
Oscar Villemaud <oscar.villemaud@epfl.ch>
2024-08-14 12:37:04 +0200

    sort keymaps by similarity

--- lua/my_keymaps/init.lua
+++ lua/my_keymaps/init.lua
@@ -1,10 +1,9 @@
+-- NAVIGATION
+
+vim.keymap.set('n', '<leader><tab>', '<C-6>', { desc = 'alternate file' })
+
 -- remap to jump to the mark instead of beginning of line
 vim.keymap.set('n', "'", '`', { noremap = true, silent = false })
-
-vim.keymap.set('n', '<C-s>', '<cmd>update<cr>', { desc = 'update (save)' })
-vim.keymap.set({ 'i', 'v' }, '<C-s>', '<cmd>update<cr><Esc>', { desc = 'come back to normal and update (save)' })
--- vim.keymap.set('n', '<leader>x', ':term open %<CR> <C-o>', { desc = 'e[X]ecute buffer' })
-vim.keymap.set('n', '<leader>br', ':edit<cr>', { desc = '[B]uffer [R]eload' })
 
 -- Keybinds to make split navigation easier.
 --  Use Superkey+<hjkl> to switch between windows
@@ -14,15 +13,19 @@ vim.keymap.set('n', '<Right>', '<C-w><C-l>', { desc = 'Move focus to the right w
 vim.keymap.set('n', '<Down>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<Up>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('n', '<leader><tab>', '<C-6>', { desc = 'alternate file' })
-
 -- change split sizes
 vim.keymap.set('n', '<C-S-l>', '<C-w><C-char-62>', { desc = 'increase split width' })
 vim.keymap.set('n', '<C-S-h>', '<C-w><C-char-60>', { desc = 'decrease split width' })
 vim.keymap.set('n', '<C-S-k>', '<C-w>+', { desc = 'increase split hight' })
 vim.keymap.set('n', '<C-S-j>', '<C-w>-', { desc = 'decrease split hight' })
 
--- TOGGLE DIAGNOSTICS
+-- BUFFER
+vim.keymap.set('n', '<C-s>', '<cmd>update<cr>', { desc = 'update (save)' })
+vim.keymap.set({ 'i', 'v' }, '<C-s>', '<cmd>update<cr><Esc>', { desc = 'come back to normal and update (save)' })
+vim.keymap.set('n', '<leader>br', ':edit<cr>', { desc = '[B]uffer [R]eload' })
+
+-- TOGGLE
+
 -- command to toggle inline diagnostics
 vim.api.nvim_create_user_command('DiagnosticsToggleVirtualText', function()
   local current_value = vim.diagnostic.config().virtual_text
@@ -48,10 +51,12 @@ vim.api.nvim_set_keymap('n', '<Leader>td', ':lua vim.cmd("DiagnosticsToggle")<CR
 vim.api.nvim_set_keymap('n', '<Leader>ti', ':lua vim.cmd("DiagnosticsToggleVirtualText")<CR>', { noremap = true, desc = '[T]oggle [I]nline diagnostics' })
 
 -- toogle spellcheck
-vim.keymap.set('n', '<Leader>ts', function()
+vim.keymap.set('n', '<Leader>tS', function()
   vim.o.spell = not vim.o.spell
   print('spell: ' .. tostring(vim.o.spell))
 end, { desc = '[T]oogle [S]pellcheck' })
+
+-- OTHER
 
 -- faster code comment
 vim.keymap.set('n', '<Leader>k', 'gcc', { remap = true, silent = false })
