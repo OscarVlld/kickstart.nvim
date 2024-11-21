5c5d0b64c74196447b6dffc008d802fd9d09a564
Oscar Villemaud <oscar.villemaud@epfl.ch>
2024-11-21 15:02:00 +0100

    added .sage ftplugin

--- ftplugin/sage.lua
+++ ftplugin/sage.lua
@@ -1,5 +1,3 @@
-""" specific config for .sage files """
-
 local map = vim.keymap.set
 
 map('n', '<leader>p', ':vsplit<CR>:term sage %<CR><C-w>h', { desc = '[P]lay (run sage file)', silent = true })
